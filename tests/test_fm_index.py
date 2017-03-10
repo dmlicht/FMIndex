bf1d07370c5353fefa7fa32ef48f8cf117f56588
David Lichtenberg <david.m.lichtenberg@gmail.com>
2017-03-10 15:08:51 -0800

    move tests to their own directory

---
+++ tests/test_fm_index.py
@@ -0,0 +1,61 @@
+from collections import Counter
+
+import pytest
+
+import fmindex
+from fmindex import n_less_than, FMIndex
+from util import find_all
+
+
+@pytest.fixture
+def example_string():
+    return "Tomorrow_and_tomorrow_and_tomorrow"
+
+
+@pytest.fixture
+def example_text():
+    with open('tests/ipsum.txt') as f:
+        return f.read()
+
+
+@pytest.fixture
+def index(example_string):
+    return FMIndex(example_string)
+
+
+def test_create_suffix_array_naive():
+    example = "abacad"
+    suffix_array = fmindex.create_suffix_array_naive(example)
+    expected = [0, 2, 4, 1, 3, 5]
+    assert expected == suffix_array
+
+
+def test_n_less_than(example_string):
+    counter = Counter(example_string)
+    occ_lex_lower = n_less_than(example_string)
+    sorted_counts = sorted(counter.items())
+    for i in range(len(sorted_counts) - 1):
+        current_letter = sorted_counts[i][0]
+        next_letter = sorted_counts[i + 1][0]
+        current_letter_count = sorted_counts[i][1]
+        diff = occ_lex_lower[next_letter] - occ_lex_lower[current_letter]
+        assert current_letter_count == diff
+
+
+def test_naive_find_all():
+    assert len(list(find_all("ababa", "aba"))) == 2
+
+
+def test_find(example_string):
+    index = FMIndex("Tomorrow_and_tomorrow_and_tomorrow")
+    assert len(index.find("tomorrow")) == 2
+
+
+def test_find_on_ipsum(example_text):
+    index = FMIndex(example_text)
+    words = example_text.split()
+    distinct_words = set(words)
+
+    naive_occurrences = {word: len(list(find_all(example_text, word))) for word in distinct_words}
+    fm_occurrences = {word: len(index.find(word)) for word in distinct_words}
+    assert fm_occurrences == naive_occurrences
---
+++ tests/test_fm_seq_set.py
@@ -0,0 +1,51 @@
+import pytest
+
+import fmindex
+import random
+
+END_CHAR = '$'
+
+
+def split_into_reads(seq, cov=10, read_len=20, skip_size=5):
+    """returns an array of simulated reads from genome sequence"""
+    generated_reads = [seq[i:i + read_len] for i in range(0, len(seq) - read_len, 5) for _ in range(cov)]
+    random.shuffle(generated_reads)
+    return generated_reads
+
+
+@pytest.fixture(scope='module')
+def reads():
+    with open('test_files/phiX174_genome.txt') as f:
+        data = f.read()
+    return split_into_reads(data, 2, 30, 10)
+
+
+@pytest.fixture(scope="session")
+def words():
+    return ["hello", "world", "im", "david"]
+
+
+@pytest.fixture
+def joined_words(words):
+    return END_CHAR.join(words) + END_CHAR
+
+
+@pytest.fixture
+def index(joined_words):
+    return fmindex.FMIndex(joined_words)
+
+
+def test_get_read_at_offset(words, index):
+    read_starts_at = 0
+    read_ends_at = -1
+    for ii in range(len(words)):
+        read_ends_at = read_ends_at + len(words[ii]) + 1
+        rand_pos_in_read = random.randint(read_starts_at, read_ends_at)
+        ith_read_from_fm = index.get_read_at_offset(rand_pos_in_read)
+        assert words[ii] == ith_read_from_fm, "problem with read " + str(ii) + ":" + words[ii] + '\n' + ith_read_from_fm
+        read_starts_at = read_starts_at + len(words[ii]) + 1
+
+
+# def test_get_nth_read(words, index):
+#     for ii, word in enumerate(words):
+#         assert index.get_nth_read(ii) == word
